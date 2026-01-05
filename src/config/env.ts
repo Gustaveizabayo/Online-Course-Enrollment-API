87838355079fac8654ae76d26e31a6e2c6e12f80
Gustave Izabayo <izabayogustave54@gmail.com>
2026-01-05 22:58:55 +0200

    Delete src directory

--- src/config/env.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const env = {
-  port: process.env.PORT || 5000,
-  jwtSecret: process.env.JWT_SECRET || "supersecretjwt",
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/online_course",
-  postgresUrl: process.env.DATABASE_URL
-};
